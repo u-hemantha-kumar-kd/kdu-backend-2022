dc6ea189126ad6e72fd5750b944ec3d38d62976a
U Hemantha Kumar <u.hemantha.kumar@kickdrumtech.com>
2022-07-11 14:01:12 +0530

    update singleton in program 1

--- homeworks/2/Program1.java
+++ homeworks/2/Program1.java
@@ -1,40 +1,34 @@
 package homework2;
 
 class Credentials{
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
+    private String name ;
+    private int count;
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getCount() {
+        return count;
     }
 
-    private String name ;
-    private int count;
-
-    //since we are using a private constructor, object cannot be instantiated,so we create a public constructor which supplies the default values to the private constructor
-    public Credentials(){
-        this("Hemanth", 10);
+    //    static Credentials obj = new Credentials();
+    private Credentials(){
+        this.name = "Hemanth";
+        this.count = 20;
     }
 
-    private Credentials(String name, int count){
-        this.name = name;
-        this.count = count;
+
+    public static Credentials getInstance()
+    {
+        return new Credentials();
     }
 
 }
 
 public class Program1 {
     public static void main(String[] args) {
-        Credentials cr = new Credentials();
+        Credentials cr = Credentials.getInstance();
         System.out.println("Name: "+cr.getName());
         System.out.println("Count: "+cr.getCount());
     }
