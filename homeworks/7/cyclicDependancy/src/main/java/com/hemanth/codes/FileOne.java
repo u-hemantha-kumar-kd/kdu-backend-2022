59922c59674587b3ee71e0c2b49fff38ed026c95
U Hemantha Kumar <u.hemantha.kumar@kickdrumtech.com>
2022-07-17 16:37:47 +0530

    Created a cyclic dependancy problem

---
+++ homeworks/7/cyclicDependancy/src/main/java/com/hemanth/codes/FileOne.java
@@ -0,0 +1,11 @@
+package com.hemanth.codes;
+
+public class FileOne {
+	FileTwo filetwo;
+
+	
+	public FileOne(FileTwo filetwo)
+	{
+		this.filetwo = filetwo; 
+	}
+}
---
+++ homeworks/7/cyclicDependancy/src/main/java/com/hemanth/codes/FileTwo.java
@@ -0,0 +1,9 @@
+package com.hemanth.codes;
+
+public class FileTwo {
+	FileOne fileone;
+
+	public FileTwo(FileOne fileone) {
+		this.fileone = fileone;
+	}
+}
